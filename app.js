a290912e496b103cc4376e5a7e1e15a187b22a5c
haneypatel <haneypatel7@gmail.com>
2024-05-07 12:54:08 +0530

    start

---
+++ app.js
@@ -0,0 +1,21 @@
+//const heading = React.createElement("h1", {using this object you put and attribute on in}, "Hello World!");
+// const parent = React.createElement("h1", {id:"heading"}, "Hello World!");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(parent);
+
+
+
+const parent=React.createElement("div",{id:"parent"},
+    React.createElement("div",{id :"child"},
+       [ React.createElement("h1",{id:"h1"},"hello from h1"),
+        React.createElement("h2",{id:"h2"},"hwllo from h2")]
+    )
+);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
+
+
